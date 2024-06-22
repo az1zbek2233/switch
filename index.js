a7b2d261a487f66f63b3b894c223d9a5bd243bfb
Azizbek Rustamov <rystamov2233@gmail.com>
2024-06-22 13:41:42 +0500

    switch

---
+++ index.js
@@ -0,0 +1,43 @@
+let day = Number(prompt("son kiriting"));
+
+switch (day) {
+    case 1:
+        alert(`1 -> dushanba`);
+        break;
+    case 2:
+        alert(`2 -> seshanba`);
+        break;
+    case 3:
+        alert(`3 -> chorshanba`);
+        break;
+    case 4:
+        alert(`4 -> payshanba`);
+        break;
+    case 5:
+        alert(`5 -> juma`);
+        break;
+    case 6:
+        alert(`6 -> shanba`);
+        break;
+    case 7:
+        alert(`7 -> yakshanba`);
+        break; 
+    default:
+         alert("error")                       
+}
+
+// if (day === "monday") {
+//     alert(`monday -> dushanba`)
+// } else if (day === "tuesday") {
+//     alert(`tuesday -> seshanba`)
+// } else if (day === "wendesday") {
+//     alert(`wendesday -> chorshanba`)
+// } else if (day === "thursday") {
+//     alert(`thursday -> payshanba`)
+// } else if (day === "friday") {
+//     alert(`friday -> juma`)
+// } else if (day === "soturday") {
+//     alert(`soturday -> shanba`)
+// } else if (day === "sunday") {
+//     alert(`sunday -> yakshanba`)
+// }
